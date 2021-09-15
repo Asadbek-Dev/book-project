269a2cfad01096eeb16c98d9882eb18fe19d0650
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-09-15 12:54:32 +0500

    bug fixed

--- src/components/Books.js
+++ src/components/Books.js
@@ -19,8 +19,8 @@ export default class Books extends Component {
             .get("https://www.googleapis.com/books/v1/volumes")
             .query({q:this.state.searchField})
             .then((data)=>{
-                // console.log(data);
-                this.setState({books:[...data.body.items]})
+                const cleanData=this.cleanData(data);
+                this.setState({books:cleanData})
             })
     }
     handleSearch = (e) => {
@@ -30,11 +30,31 @@ export default class Books extends Component {
         console.log(e.target.value);
         this.setState=({sort:e.target.value})
     }
+    cleanData=(data)=>{
+        const cleanedData=data.body.items.map((book)=>{
+            if(book.volumeInfo.hasOwnProperty('publishedDate')===false){
+                book.volumeInfo['publishedDate']='0000';
+            }
+            else if(book.volumeInfo.hasOwnProperty('imageLinks')===false){
+                book.volumeInfo['imageLinks']={thumbnail:'https://vignette.wikia.nocookie.net/pandorahearts/images/a/ad/Not_available.jpg/revision/latest?cb=20141028171337'}
+            }
+            return book;
+        })
+        return cleanedData
+    }
     render() {
+        const sortedBooks=this.state.books.sort((a,b)=>{
+            if(this.state.sort === 'Newest'){
+                return parseInt(b.volumeInfo.publishedDate.substring(0,4))-parseInt(a.volumeInfo.publishedDate.substring(0,4))
+            }
+            else if(this.state.sort === 'Oldest'){
+                return parseInt(a.volumeInfo.publishedDate.substring(0,4))-parseInt(b.volumeInfo.publishedDate.substring(0,4))
+            }
+        })
         return (
             <div>
                 <SearchArea searchBook={this.searchBook} handleSort={this.handleSort} handleSearch={this.handleSearch} />
-                <BookList books={this.state.books}/>
+                <BookList books={sortedBooks}/>
             </div>
         )
     }
